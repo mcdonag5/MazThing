df2b62713d76eb76515431b78c732037ab5a9ede
mcdonag5 <44897074+mcdonag5@users.noreply.github.com>
2018-11-12 22:33:52 +0000

    First Upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonWest = new System.Windows.Forms.Button();
+            this.buttonSouth = new System.Windows.Forms.Button();
+            this.buttonEast = new System.Windows.Forms.Button();
+            this.buttonNorth = new System.Windows.Forms.Button();
+            this.labelRoomDescription = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonWest
+            // 
+            this.buttonWest.Enabled = false;
+            this.buttonWest.Location = new System.Drawing.Point(12, 226);
+            this.buttonWest.Name = "buttonWest";
+            this.buttonWest.Size = new System.Drawing.Size(75, 23);
+            this.buttonWest.TabIndex = 1;
+            this.buttonWest.Text = "West";
+            this.buttonWest.UseVisualStyleBackColor = true;
+            this.buttonWest.Click += new System.EventHandler(this.buttonWest_Click);
+            // 
+            // buttonSouth
+            // 
+            this.buttonSouth.Location = new System.Drawing.Point(93, 226);
+            this.buttonSouth.Name = "buttonSouth";
+            this.buttonSouth.Size = new System.Drawing.Size(75, 23);
+            this.buttonSouth.TabIndex = 2;
+            this.buttonSouth.Text = "South";
+            this.buttonSouth.UseVisualStyleBackColor = true;
+            this.buttonSouth.Click += new System.EventHandler(this.buttonSouth_Click);
+            // 
+            // buttonEast
+            // 
+            this.buttonEast.Enabled = false;
+            this.buttonEast.Location = new System.Drawing.Point(174, 226);
+            this.buttonEast.Name = "buttonEast";
+            this.buttonEast.Size = new System.Drawing.Size(75, 23);
+            this.buttonEast.TabIndex = 3;
+            this.buttonEast.Text = "East";
+            this.buttonEast.UseVisualStyleBackColor = true;
+            this.buttonEast.Click += new System.EventHandler(this.buttonEast_Click);
+            // 
+            // buttonNorth
+            // 
+            this.buttonNorth.Enabled = false;
+            this.buttonNorth.Location = new System.Drawing.Point(93, 197);
+            this.buttonNorth.Name = "buttonNorth";
+            this.buttonNorth.Size = new System.Drawing.Size(75, 23);
+            this.buttonNorth.TabIndex = 4;
+            this.buttonNorth.Text = "North";
+            this.buttonNorth.UseVisualStyleBackColor = true;
+            this.buttonNorth.Click += new System.EventHandler(this.buttonNorth_Click);
+            // 
+            // labelRoomDescription
+            // 
+            this.labelRoomDescription.AutoSize = true;
+            this.labelRoomDescription.Location = new System.Drawing.Point(5, 21);
+            this.labelRoomDescription.MaximumSize = new System.Drawing.Size(240, 0);
+            this.labelRoomDescription.Name = "labelRoomDescription";
+            this.labelRoomDescription.Size = new System.Drawing.Size(192, 13);
+            this.labelRoomDescription.TabIndex = 5;
+            this.labelRoomDescription.Text = "To the south is a entryway to the maze.";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labelRoomDescription);
+            this.panel1.Controls.Add(this.buttonNorth);
+            this.panel1.Controls.Add(this.buttonSouth);
+            this.panel1.Controls.Add(this.buttonEast);
+            this.panel1.Controls.Add(this.buttonWest);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(390, 270);
+            this.panel1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonWest;
+        private System.Windows.Forms.Button buttonSouth;
+        private System.Windows.Forms.Button buttonEast;
+        private System.Windows.Forms.Button buttonNorth;
+        private System.Windows.Forms.Label labelRoomDescription;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        
+        static string[,,] rooms = new string[6, 6,3] {
+            {{"SE", "",""},{"SW","","" },{"ES", "The entryway collapses behind you.",""},{"EW","","" },{"SW","","" },{"S","","" } },//0
+            {{"NS", "",""},{"NE", "",""},{"NEW", "",""},{"SW", "",""},{"NS", "",""},{"NS", "",""}},//1
+            {{"NE", "",""},{"EW", "",""},{"SW", "","Normal"},{"NS", "",""},{"NES", "","Normal"},{"NW", "",""}},//2
+            {{"ES", "",""},{"EW", "","Elite"},{"NEW", "",""},{"NSW", "",""},{"NE", "",""},{"SW", "",""}},//3
+            {{"N", "",""},{"ES", "",""},{"SW", "",""},{"NE", "",""},{"ESW", "",""},{"NSW", "",""}},//4
+            {{"E", "To the west you see the exit of the maze!",""},{"NW", "",""},{"NE", "",""},{"EW", "",""},{"NW", "","Boss"},{"N", "",""}} //5
+        };
+        int playerX = 2;
+        int playerY = -1;
+        //on load event
+        private void Form1_Load(object sender, EventArgs e) {
+            
+        }
+        //update room details in form
+        private void UpdateRoom(string move) {
+            //MessageBox.Show(rooms[y, x]);
+            //change x and y
+            switch (move) {
+                case "North":
+                    playerY--;
+                    break;
+                case "East":
+                    playerX++;
+                    break;
+                case "South":
+                    playerY++;
+                    break;
+                case "West":
+                    playerX--;
+                    break;
+                default:
+                    break;
+            }
+            //buttons
+            buttonNorth.Enabled = rooms[playerY,playerX,0].Contains("N") ? true : false;
+            buttonEast.Enabled = rooms[playerY, playerX, 0].Contains("E") ? true : false;
+            buttonSouth.Enabled = rooms[playerY, playerX, 0].Contains("S") ? true : false;
+            buttonWest.Enabled = rooms[playerY, playerX, 0].Contains("W") ? true : false;
+            //room description
+            labelRoomDescription.Text = rooms[playerY, playerX, 1];
+            if (playerX==2 && playerY==0) {
+                rooms[0, 2, 1] = "The entryway to the north is blocked.";
+            }
+            //monster fight check
+            if(rooms[playerX,playerY,2] != "") {
+                switch (rooms[playerX,playerY,2]) {
+                    case "Normal":
+
+                        break;
+                    case "Elite":
+
+                        break;
+                    case "Boss":
+
+                        break;
+                }
+            }
+
+        }
+
+        //change views
+        private void ChangeState(string state) {
+            switch(state) {
+                case "Map":
+                    MapState(true);
+                    FightState(false);
+                    break;
+                case "Fight":
+                    FightState(true);
+                    MapState(false);
+                    break;
+            }
+        }
+        //Map view state
+        private void MapState(bool b) {
+            labelRoomDescription.Visible = b;
+            buttonNorth.Visible = b;
+            buttonEast.Visible = b;
+            buttonSouth.Visible = b;
+            buttonWest.Visible = b;
+        }
+        //Fight view state
+        private void FightState(bool b) {
+
+        }
+        //direction buttons
+        private void buttonNorth_Click(object sender, EventArgs e) => UpdateRoom("North");
+
+        private void buttonEast_Click(object sender, EventArgs e) => UpdateRoom("East");
+
+        private void buttonSouth_Click(object sender, EventArgs e) => UpdateRoom("South");
+
+        private void buttonWest_Click(object sender, EventArgs e) => UpdateRoom("West");
+    }
+}
